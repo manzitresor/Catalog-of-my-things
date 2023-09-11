d81b4e8391707573692b20da44af9739f735d154
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-09-11 17:03:22 +0300

    Create item class.

--- item_content/item.rb
+++ item_content/item.rb
@@ -1,27 +1,28 @@
+require 'date'
 class Item
-    attr_accessor :genre, :author, :source, :label, :publish_date,
+  attr_accessor :genre, :author, :source, :label, :publish_date
 
-    def initialize(id,genre,author,source,label,publish_date,archived)
-      @id = id,
-      @genre = genre,
-      @author = author,
-      @source = source,
-      @label = label,
-      @publish_date = publish_date,
-      @archived = archived
-    end
+  def initialize(genre, author, source, label)
+    @id = rand(1..1000)
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = Date.today
+    @archived = true
+  end
 
-    private
-    def can_be_archived?
-        @publish_date > 10 ? true : false
-    end
+  private
 
-    public
-    def move_to_archive
-        if self.can_be_archived? == true
-            @archived = true
-        end
-    end
-    
-    
+  def can_be_archived?
+    @publish_date > 10
+  end
+
+  public
+
+  def move_to_archive
+    return unless can_be_archived? == true
+
+    @archived = true
+  end
 end

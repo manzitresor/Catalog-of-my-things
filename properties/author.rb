529ff89864fd6b2951e3b69ba1c07f9aa9531317
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-09-11 17:26:04 +0300

    Add author class.

---
+++ properties/author.rb
@@ -0,0 +1,16 @@
+require 'json'
+class Author
+    attr_accessor :first_name, :last_name
+    def initialize(first_name,last_name)
+        @id = rand(1..1000)
+        @first_name = first_name
+        @last_name = last_name
+        @items = []
+    end
+
+    def add_item(item)
+        item.author = JSON.parse(self.to_json)
+        @items << item
+        
+    end
+end

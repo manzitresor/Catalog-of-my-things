19fc287ebfe2f7f839e3a083ed27bbc5d451d185
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-09-11 19:07:20 +0200

    add rubocop collector

--- item_content/label.rb
+++ item_content/label.rb
@@ -1,15 +1,17 @@
-require_relative'../app_helper'
+require_relative '../app_helper'
 
 class Label < Item
-    attr_accessor :title, :color, :items
-    def initialize(id,title,color,items)
-        super()
-        @id = Random.rand(1..1000)
-        @title = title
-        @color = color
-        @items = []
-    end
-    def add_item=(item)
-        @items << item unless @items.include?(item)
-    end
+  attr_accessor :title, :color, :items
+
+  def initialize(_id, title, color, _items)
+    super()
+    @id = Random.rand(1..1000)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item=(item)
+    @items << item unless @items.include?(item)
+  end
 end

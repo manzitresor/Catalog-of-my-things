f18e44f4352b4af0e67d65364bc636e00b08249c
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-09-11 18:01:29 +0200

    add method define Book class

--- item_content/book.rb
+++ item_content/book.rb
@@ -1,5 +1,12 @@
-class Book
-    def initialize
-        
+class Book < Item
+    attr_accessor :publisher, :cover_state
+    def initialize(publisher,cover_state)
+        super(publish_date)
+        @publisher = publisher
+        @cover_state = cover_state
+    end
+    private
+    def can_be_archieved?()
+        super | @cover_state == 'bad'
     end
 end

b5d5d9e537d2b29994f3d8533a3f2cfd56e65401
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-09-11 17:24:38 +0300

    Add game class.

---
+++ item_content/game.rb
@@ -0,0 +1,23 @@
+require_relative 'item'
+
+class Game < Item
+    attr_accessor :multiplayer, :last_played_at, :author, :genre, :source, :label
+    @game_list = []
+    def initialize(multiplayer, last_played_at)
+        @multiplayer = multiplayer
+        @last_played_at = last_played_at
+        self.class.game_list << self
+    end
+
+    private
+    def can_be_archived?
+        self.class.can_be_archived? == true && @last_played_at > 2 ? true : false
+    end
+
+    public
+
+    class << self
+      attr_accessor :game_list
+    end
+
+end

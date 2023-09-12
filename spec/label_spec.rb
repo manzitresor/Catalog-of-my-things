3e04d46edccb49eda17d2310c2828dea94823bff
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-09-12 14:26:57 +0200

    add rubcop correction

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,28 +1,28 @@
 require_relative '../app_helper'
 
 describe Label do
-    before :each do
-        @label = Label.new("react","orange")
-        @item = Item.new('20-3-2023')
+  before :each do
+    @label = Label.new('react', 'orange')
+    @item = Item.new('20-3-2023')
+  end
+  describe 'Initialize parameters' do
+    it 'throws an argumentError when given less than 2 arguments' do
+      expect { Label.new 'react' }.to raise_error(ArgumentError)
     end
-    describe "Initialize parameters" do
-        it 'throws an argumentError when given less than 2 arguments' do
-            expect { Label.new "react"}.to raise_error(ArgumentError)
-        end
+  end
+  describe '#add_item' do
+    it 'should have empty array' do
+      expect(@label.items).to be_empty
     end
-    describe "#add_item" do
-        it 'should have empty array' do
-            expect(@label.items).to be_empty
-        end
 
-        it 'add item in items array' do
-            @label.add_item = @item
-            expect(@label.items).to include(@item)
-        end
-        it 'Does not add item twice' do
-            @label.add_item = @item
-            @label.add_item = @item
-            expect(@label.items.count(@item)).to eq(1)
-        end
+    it 'add item in items array' do
+      @label.add_item = @item
+      expect(@label.items).to include(@item)
     end
-end 
+    it 'Does not add item twice' do
+      @label.add_item = @item
+      @label.add_item = @item
+      expect(@label.items.count(@item)).to eq(1)
+    end
+  end
+end

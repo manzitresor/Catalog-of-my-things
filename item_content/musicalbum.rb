ccf7c5cc1ba17dc85dba98615526957a967eb0e9
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-09-12 21:54:09 +0500

    linters error fixed

--- item_content/musicalbum.rb
+++ item_content/musicalbum.rb
@@ -1,16 +1,17 @@
 require_relative 'item'
 
 class MusicAlbum < Item
-    attr_accessor :on_spotify
-    def initialize(on_spotify)
-        super("Music", "Unknown", "Unknown", "Unknown")
-        @on_spotify = on_spotify
-    end
+  attr_accessor :on_spotify
 
-    private
+  def initialize(on_spotify)
+    super('Music', 'Unknown', 'Unknown', 'Unknown')
+    @on_spotify = on_spotify
+  end
 
-    def can_be_archieved?()
-        parent_can_be_archived = super
-        parent_can_be_archived && @on_spotify
-    end
+  private
+
+  def can_be_archieved?()
+    parent_can_be_archived = super
+    parent_can_be_archived && @on_spotify
+  end
 end

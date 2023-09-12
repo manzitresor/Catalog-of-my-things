125fce7897c6ac31906953af8107f2c3ceb2495b
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-09-12 22:33:26 +0500

    add_item method in genre class

--- item_content/genre.rb
+++ item_content/genre.rb
@@ -6,5 +6,15 @@ class Genre
       @id = Random.rand(1..1000)
       @items = []
     end
+
+    def add_item(item)
+        if item.is_a?(Item)
+          @items << item
+          item.genre = self 
+        else
+          puts "Invalid item. Please provide an instance of the Item class."
+        end
+    end
+
 end
   

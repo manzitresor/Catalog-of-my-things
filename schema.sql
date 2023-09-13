dee00a45e104c8b8cfd532a02d1e3da0c3bc3491
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-09-13 16:22:31 +0300

    Added game and author table.

--- schema.sql
+++ schema.sql
@@ -1,3 +1,23 @@
+/* Database schema to keep the structure of entire database. */
+
+CREATE DATABASE catalog_keeper;
+
+/* Create games table. */
+
+CREATE TABLE games( 
+    id INT GENERATED ALWAYS AS IDENTITY,
+    publisher VARCHAR(250),
+    cover_state VARCHAR(250),
+    author_id INT REFERENCES author(id),
+    publish_date DATE,
+    archived boolean,
+    PRIMARY KEY (id),
+ );
+
+/* Create author table. */
+
+ CREATE TABLE author(id INT GENERATED ALWAYS AS IDENTITY, first_name VARCHAR(250), last_name VARCHAR(250), PRIMARY KEY (id));
+
 CREATE TABLE Book(
 publisher VARCHAR(50),
 cover_state VARCHAR(50)

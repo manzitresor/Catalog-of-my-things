67f37a2f245b289ed562e31b2ee013f2102157e2
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-09-13 14:32:34 +0300

    Add tests for Author class.

--- app_helper.rb
+++ app_helper.rb
@@ -1,4 +1,6 @@
 require_relative 'item_content/item'
 require_relative 'item_content/book'
-require_relative 'item_content/label'
+require_relative 'properties/label'
 require_relative 'item_content/genre'
+require_relative 'item_content/game'
+require_relative 'properties/author'
